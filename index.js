12c5d3396f475e7cce129278c1d88b8615a247bb
Austin Jess <ajess33@gmail.com>
2019-03-08 15:09:51 -0600

    added icon and more

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const el = document.getElementById('hello');
+// const el = document.getElementById('hello');
 
 const name = prompt('What is your name?');
-
-console.log(el);
-
-// if (name != '') {
-
-// }
