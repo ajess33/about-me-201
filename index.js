26c210f98913b9133ce535f2b51e8f83a804de9d
Austin Jess <ajess33@gmail.com>
2019-03-08 16:33:43 -0600

    playing with javascript

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-// const el = document.getElementById('hello');
-
 const name = prompt('What is your name?');
+const intro = document.querySelector('.hello');
+console.log(name);
+console.log(intro);
