ac4d0cfe9b13d84b1a56f8693d71540228433054
Austin Jess <ajess33@gmail.com>
2019-03-12 10:05:54 -0500

    deleted nav button borders

--- js/about-me.js
+++ js/about-me.js
@@ -2,13 +2,17 @@ const playButton = document.getElementById('start-game');
 playButton.addEventListener('click', guessingGame);
 
 function guessingGame() {
-  var number = Math.floor(Math.random() * 10);
+  var number = Math.floor(Math.random() * 20);
   console.log(number);
-  alert('I have a number between 1-10. Ready to play?');
+  alert('I have a number between 1-20. Ready to play?');
   var guess = parseFloat(prompt('Guess a number'));
 
   while (guess !== number) {
-    guess = parseFloat(prompt('Your guess is wrong. Try again!!'));
+    if (guess > number) {
+      guess = parseFloat(prompt('You guessed TOO HIGH. Try again'));
+    } else if (guess < number) {
+      guess = parseFloat(prompt('You guessed TOO LOW. Try again'));
+    }
   }
   if (guess === number) {
     alert(`Congrats ${guess} was the number!!`);
